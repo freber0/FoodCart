69ddc623e1f8219d375b0b74d5ad03d52128a4a8
Frederic Bernard <frederic.bernard.3@ulaval.ca>
2019-03-12 16:14:30 -0400

    add item to cart route api

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/cart/<id>')
+def add_to_cart(id):
+    return f"Ajout du produit avec le id {id} au panier"
+
 if __name__ == '__main__':
     app.run()

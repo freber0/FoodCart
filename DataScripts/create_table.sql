95c0d32cd2d080f57dc0d19f031bd966cb5ba018
Alex Raymond <alex.raymond11@icloud.com>
2019-04-05 15:04:11 -0400

    Some fixes

--- DataScripts/create_table.sql
+++ DataScripts/create_table.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE IF NOT EXISTS FoodCart;
 use FoodCart
 DROP TABLE IF EXISTS products;
-CREATE TABLE products(product_id integer, class_name varchar(20), name varchar(25), price float, image varchar(125));
+CREATE TABLE products(product_id integer, class_name varchar(20), name varchar(25), price float, image varchar(130));

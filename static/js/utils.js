3160f18621bd039218b3c9cc4b61eaa2cdf05b2d
Frederic Bernard <frederic.bernard.3@ulaval.ca>
2019-04-09 10:04:29 -0400

    added cart post and delete route

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,5 +1,4 @@
 function addToCart(id, qte) {
-    console.log("function HELLO?")
     let header = new Headers();
     header.append("Content-Type", "application/json");
     let init = {
@@ -10,6 +9,19 @@ function addToCart(id, qte) {
         body: JSON.stringify({quantity: qte})
     };
     fetch("/cart/"+id, init).then(function(data) {
-        console.log("callback?");
+        //processing after?
+    });
+}
+
+function removeFromCart(id) {
+    let header = new Headers();
+    header.append("Content-Type", "application/json");
+    let init = {
+        credentials: 'same-origin',
+        method: 'DELETE',
+        headers: header,
+    };
+    fetch("/cart/"+id, init).then(function(data) {
+    //processing after?
     });
 }

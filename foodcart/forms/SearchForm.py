ff85dd4e5f803a76e46a33cffbb4e4ac6df78777
bidetzz <antoine1adam@hotmail.com>
2019-04-10 00:48:12 -0400

    cart working

--- foodcart/forms/SearchForm.py
+++ foodcart/forms/SearchForm.py
@@ -5,7 +5,6 @@ from flask import request
 def search():
     search_value = request.form['search']
     cursor.execute("USE FoodCart")
-    print(search_value)
     cursor.execute("SELECT * FROM products WHERE name LIKE '%" + search_value + "%' OR class_name='" + search_value + "'")
     searched_products = cursor.fetchall()
     return searched_products

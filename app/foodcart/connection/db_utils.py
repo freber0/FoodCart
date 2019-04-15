efea64c8f82c0dfd829b191ee4f126ec2da7e544
bidetzz <antoine1adam@hotmail.com>
2019-04-14 23:57:09 -0400

    button added for checkout and code cleaning

--- app/foodcart/connection/db_utils.py
+++ app/foodcart/connection/db_utils.py
@@ -1,16 +1,11 @@
 from mysql.connector import connect
-import time
-
-# un sleep de 30s pour s'assurer que la bd soit bien starter avant que l'app essaie de se connecter avec le docker-compose up
-
-# time.sleep(30)
 
 # Sert a faire la connection avec la BD
 mydb = None
+
 while mydb is None:
     try:
         mydb = connect(
-            #host="localhost",
             host="db",
             user="root",
             password="1234",
